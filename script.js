14b3e356f70884a58eb5abfe51ab2aeeb3046196
saitoi <pedrohhs@dcc.ufrj.br>
2024-01-26 17:31:24 -0300

    Adicionando screenshots

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const searchInput = document.getElementById('search-input');
-const resultsArtist = document.getElementById('result-artist');
+const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
@@ -9,25 +9,26 @@ function requestApi(searchTerm) {
         .then((result) => displayResults(result))
 }
 
-document.addEventListener('input', function() {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultsArtist.classList.remove('hidden');
-        return;
-    }
-})
-
 function displayResults(result) {
-    resultPlaylist.classList.add('hidden');
+    resultPlaylist.classList.add("hidden")
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
     result.forEach(element => {
         artistName.innerText = element.name;
-        artistImage.src = element.picture;
+        artistImage.src = element.urlImg;
     });
 
-    resultsArtist.classList.remove('hidden');
-    
+    resultArtist.classList.remove('hidden');
 }
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
+})
